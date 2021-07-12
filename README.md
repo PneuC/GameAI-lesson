cb285fbf183f521c4ce4d7515fbc93641e58a41b
3500 <2144654576@qqcom>
2021-07-12 09:48:04 +0800

    Add command line arguments to Snake/main.py. Update README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # GameAI-lesson
+
+### Snake
+
+#### Play game:
+
+```
+python main.py
+```
+
+Use direction keys to control snake move.
+
+#### Play game with agent:
+
+```
+python main.py --use_agent
+```
+
--- Snake/main.py
+++ Snake/main.py
@@ -1,5 +1,15 @@
+import argparse
 from game import Game
+from agent import RandomAgent
 
 
 if __name__ == '__main__':
-    Game().run()
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--use_agent', action='store_true', default=False)
+
+    args = argparser.parse_args()
+
+    if args.use_agent:
+        Game(agent=RandomAgent()).run()
+    else:
+        Game().run()
